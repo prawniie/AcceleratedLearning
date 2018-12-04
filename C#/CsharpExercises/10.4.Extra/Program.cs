b71fcfddd270c15a798034a81bfa38db14b0ebf6
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2018-12-04 14:28:28 +0100

    10.4 Extrauppgift med tal istället för namn, utan riktiga beräkningar dock

---
+++ C#/CsharpExercises/10.4.Extra/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10._4.Extra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numList = AskUserForNumbers();
+            CalculateMean(numList);
+            CalculateMedian(numList);
+        }
+
+        private static List<decimal> AskUserForNumbers()
+        {
+            string input;
+            List<decimal> numList = new List<decimal>();
+
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                input = Console.ReadLine();
+
+                if (input=="exit")
+                {
+                    break;
+                }
+
+                decimal number = decimal.Parse(input);
+                numList.Add(number);
+            }
+
+            return numList;
+        }
+
+        private static void CalculateMean(List<decimal> numList)
+        {
+            Console.WriteLine("Calculating mean..");
+
+        }
+
+        private static void CalculateMedian(List<decimal> numList)
+        {
+            Console.WriteLine("Calculating median..");
+        }
+    }
+}
