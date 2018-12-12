3f319c2911c1e8ed20883590667da685ce2d0df3
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2018-12-12 08:59:42 +0100

    Module 12.1

---
+++ C#/CsharpExercises/Module12.1/Module12.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module12._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rockstarsArray = new string[] { "Bruce Dickinson", "Ozzy Osbourne", "Jim Morrison" };
+            List<string> rockstarsList = new List<string>() { "Bruce Dickinson", "Ozzy Osbourne", "Jim Morrison" };
+
+            DisplayArrayOfRockstars(rockstarsArray);
+            DisplayListOfRockstars(rockstarsList);
+
+            DisplayRockstars(rockstarsArray);
+            DisplayRockstars(rockstarsList);
+        }
+
+        private static void DisplayListOfRockstars(List<string> rockstarsList)
+        {
+            Console.WriteLine("My rockstars: (list)");
+            PrintRockstarsName(rockstarsList);
+        }
+
+        private static void DisplayArrayOfRockstars(string[] rockstarsArray)
+        {
+            Console.WriteLine("My rockstars: (array)");
+            PrintRockstarsName(rockstarsArray);
+        }
+
+        private static void DisplayRockstars(IEnumerable<string> rockstars)
+        {
+            Console.WriteLine("My rockstars: (ienumerable)");
+            PrintRockstarsName(rockstars);
+        }
+
+        private static void PrintRockstarsName(IEnumerable<string> rockstars)
+        {
+            Console.WriteLine();
+            foreach (var rockstar in rockstars)
+            {
+                Console.WriteLine("* " + rockstar);
+            }
+            Console.WriteLine();
+        }
+    }
+}
